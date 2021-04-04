b075c908dc724c73504166dd1c98bc888a4321b9
Hemant Kumar Sah <hemantanshu@gmail.com>
2021-04-05 00:17:09 +0530

    #comment initialised data with modules

--- src/utility/utility.module.ts
+++ src/utility/utility.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlatformUtilityModule } from '@servicelabsco/nestjs-utility-services';
+import es6Classes from './es6.classes';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature(es6Classes.entities),
+        PlatformUtilityModule,
+    ],
+})
 export class UtilityModule {}
